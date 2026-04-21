9a9186de4bd02195e497292f5a42c4c7095b2f5e
Mariana Emilia Sanchez Adon <20240124@itla.edu.do>
2026-04-21 16:52:36 -0400

    dark mode

--- ProyectoFinal_BibliotecaPersonal/App.xaml.cs
+++ ProyectoFinal_BibliotecaPersonal/App.xaml.cs
@@ -7,6 +7,7 @@ namespace ProyectoFinal_BibliotecaPersonal
         public App()
         {
             InitializeComponent();
+            //Application.Current.UserAppTheme = AppTheme.Dark;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
